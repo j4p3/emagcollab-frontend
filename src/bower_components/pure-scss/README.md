580a7312479433d5328131af4f7d55bd047120d4
JP <bonnerjp@gmail.com>
2014-03-20 17:42:11 -0400

    day one. initial onepager.

---
+++ src/bower_components/pure-scss/README.md
@@ -0,0 +1,4 @@
+pure-scss
+=========
+
+This is a SASS/SCSS implementation of [Pure.css](http://purecss.io/). At the moment it is based on version _3.0.0_ of the library.
---
+++ src/js/main.js
@@ -0,0 +1,44 @@
+////////////////////////////////////////////////////////////////////////////////
+// HEADER ANIMATION ONSCROLL
+////////////////////////////////////////////////////////////////////////////////
+var AnimatedHeader = (function() {
+ 
+    var docElem = document.documentElement,
+        header = document.querySelector( '.header' ),
+        didScroll = false,
+        changeHeaderOn = 10;
+ 
+    function init() {
+        if(!window.addEventListener) {
+            window.attachEvent('scroll', bufferScroll);
+        }
+        else {
+            window.addEventListener('scroll', bufferScroll, false);
+        }
+    }
+
+    function bufferScroll() {
+        if( !didScroll ) {
+            didScroll = true;
+            setTimeout( scrollPage, 250 );
+        }
+    }
+ 
+    function scrollPage() {
+        var sy = scrollY();
+        if ( sy >= changeHeaderOn ) {
+            classie.add( header, 'shrink' );
+        }
+        else {
+            classie.remove( header, 'shrink' );
+        }
+        didScroll = false;
+    }
+ 
+    function scrollY() {
+        return window.pageYOffset || docElem.scrollTop;
+    }
+ 
+    init();
+ 
+})();
